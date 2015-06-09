6f6b28670814a3a7dc1d00d266abaebe071e68c5
Odin Hole Standal <odin@odinodin.com>
2015-06-09 16:23:42 +0200

    Remove generated js files

--- bounce/resources/public/canvas-fn.js
+++
@@ -1,9 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../canvas_fn/util.js", ['canvas_fn.util'], ['cljs.core']);
-goog.addDependency("../domina/support.js", ['domina.support'], ['goog.dom', 'cljs.core', 'goog.events']);
-goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
-goog.addDependency("../domina.js", ['domina'], ['goog.dom.forms', 'goog.dom', 'goog.dom.classes', 'goog.string', 'cljs.core', 'domina.support', 'goog.style', 'clojure.string', 'goog.events', 'goog.dom.xml']);
-goog.addDependency("../canvas_fn/canvas.js", ['canvas_fn.canvas'], ['cljs.core']);
-goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventTarget', 'goog.events.EventType', 'goog.events']);
-goog.addDependency("../canvas_fn/core.js", ['canvas_fn.core'], ['canvas_fn.util', 'cljs.core', 'domina', 'canvas_fn.canvas', 'clojure.browser.event']);
--- bounce/resources/public/js/out/canvas_fn/util.cljs
+++
@@ -1,4 +0,0 @@
-(ns canvas-fn.util)
-
-(defn log [& items]
-  (.log js/console (apply str items)))
