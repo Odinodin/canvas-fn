d310f82d429516a8800062ddef0861ea31961568
Odin Hole Standal <odin@odinodin.com>
2015-08-27 22:02:32 +0200

    Show snake and apple as list

--- snake/src/canvas_fn_snake/core.cljs
+++ snake/src/canvas_fn_snake/core.cljs
@@ -10,10 +10,14 @@
 
 (enable-console-print!)
 
-(def colors {:grey-seethrough "rgba(100,100,100,0.6)"
+(def speed 5000)
+
+(def colors {:grey-seethrough "rgba(110,110,110,0.6)"
              :dark-grey       "rgb(30,30,30)"
+             :grey            "rgb(200,200,200)"
              :purple          "rgb(186,85,211)"
-             :green           "rgb(50,180,100)"
+             :green           "rgb(90,180,100)"
+             :dark-green      "rgb(50,200,80)"
              :red             "rgb(244,116,114)"
              :blue            "rgb(20,20,200)"})
 
@@ -23,6 +27,9 @@
 ;; Data to be drawn
 (def width 20)
 (def height 20)
+
+
+
 
 (def initial-state {:game-running             true
                     :snake                    [[0 0] [0 1] [0 2]]
@@ -85,10 +92,11 @@
 (defmulti draw-cell (fn [ctx cell-value x y cell-width] cell-value))
 
 (defmethod draw-cell :empty [ctx cell-value x y cell-width]
-  (canv/fill-square ctx [(* y cell-width) (* x cell-width)] (- cell-width 2) (:dark-grey colors)))
+  (canv/fill-square ctx [(* y cell-width) (* x cell-width)] (- cell-width 2) (:grey colors)))
 
 (defmethod draw-cell :snake-head [ctx cell-value x y cell-width]
-  (canv/fill-square ctx [(* y cell-width) (* x cell-width)] (- cell-width 2) (:green colors)))
+  (canv/fill-square ctx [(* y cell-width) (* x cell-width)] (- cell-width 2) (:dark-green colors
+                                                                               )))
 
 (defmethod draw-cell :snake-body [ctx cell-value x y cell-width]
   (canv/fill-square ctx [(* y cell-width) (* x cell-width)] (- cell-width 2) (:green colors)))
@@ -98,7 +106,8 @@
 
 (defn draw-board [canvas model]
   (let [ctx (.getContext canvas "2d")
-        board-with-snake (reduce (fn [board snake-coord] (assoc-in board snake-coord :snake-head)) (:board model) (:snake model))
+        board-with-snake (reduce (fn [board snake-coord] (assoc-in board snake-coord :snake-body)) (:board model) (:snake model))
+        board-with-snake (assoc-in board-with-snake (last (:snake model)) :snake-head)
         board-with-apples-and-snakes (reduce (fn [board apple-coord] (assoc-in board apple-coord :apple)) board-with-snake (:apples model))
         cell-width (:cell-width model)]
     (doseq [[row-idx row] (map-indexed (fn [idx row] [idx row]) board-with-apples-and-snakes)]
@@ -177,30 +186,57 @@
         spawn-apple)
     model))
 
-(defn render-html []
+
+(q/defcomponent Coord-row
+                [[x y]]
+                (d/tr {}
+                      (d/td {} x)
+                      (d/td {} y)))
+
+(q/defcomponent Position-table [positions title]
+                (d/table
+                  {:style {:borderCollapse "collapse"}}
+                  (d/thead
+                    {:style {:backgroundColor "grey"}}
+                    (d/tr
+                      {}
+                      (d/th {:colSpan 2} title))
+                    (d/tr
+                      {}
+                      (d/th {} "x")
+                      (d/th {} "y")))
+                  (d/tbody {}
+                           (map #(Coord-row %) positions))))
+
+(defn render-html [model]
   (q/render
     (d/div {}
            (d/h1 {:style {:color "grey"}}
-                 "snake")
-           (d/canvas {:id "draw-canvas" :width "450px" :height "450px"} ""))
+                 (str "Game over? " (not (:game-running model))))
+           (d/h1 {:style {:color "grey"}}
+                 (str "Snake direction " (:snake-direction model)))
+           (d/canvas {:id "draw-canvas" :width "450px" :height "450px"} "")
+           (str "Snake: " (seq (:snake model)))
+           (str "Apples: " (seq (:apples model)))
+           (Position-table (:snake model) "Snake")
+           (Position-table (:apples model) "Apples "))
     (.getElementById js/document "main")))
+
 
 (defn animate []
   "Main loop"
-  (do
-    (render-html)
-    (canv/animate animate)
-
-    (render-canvas (dom/by-id "draw-canvas") @model)))
-
-(def speed 2000)
+  (let [data @model]
+    (render-html data)
+    (when-let [canvas (dom/by-id "draw-canvas")]
+      (render-canvas canvas data))
+    (canv/animate animate)))
 
 ;; Start the game
 (defonce game-loop
-         (go-loop []
-                  (<! (timeout speed))
-                  (swap! model update-model)
-                  (recur)))
+         (do
 
-(defonce u (animate))
-
+           (go-loop []
+                    (<! (timeout speed))
+                    (swap! model update-model)
+                    (recur))
+           (animate)))
